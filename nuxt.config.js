970fd1cd9458f3756a006b4c096f80394997724c
kenan dufrene <kenan.dufrene@live.fr>
2021-05-21 05:45:01 +0200

    seo optimisation og + twitter

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,32 @@
 export default {
-  head: {
-    title: 'Kenan DUFRENE - Portfolio',
-    htmlAttrs: {
-      lang: 'fr'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "Passionné par la programmation depuis mon enfance, je me suis orienté dans le domaine du web et plus particulièrement sur les technologies Javascript." },
-      // twitter
-      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
-      { hid: 'twitter:title', name: 'twitter:title', content: 'Kenan DUFRENE - Portfolio' },
-      { hid: 'twitter:description', name: 'twitter:description', content: "Passionné par la programmation depuis mon enfance, je me suis orienté dans le domaine du web et plus particulièrement sur les technologies Javascript." },
-      { hid: 'twitter:image', name: 'twitter:image', content: '~/assets/uploads/og-image.png' },
-      { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: 'Kenan DUFRENE - Portfolio' },
-      /// open-graph
-      { hid: 'og:title', property: 'og:title', content: 'Kenan DUFRENE - Portfolio' },
-      { hid: 'og:description', property: 'og:description', content: "Passionné par la programmation depuis mon enfance, je me suis orienté dans le domaine du web et plus particulièrement sur les technologies Javascript." },
-      { hid: 'og:image', property: 'og:image', content: '~/assets/uploads/og-image.png' },
-      { hid: 'og:image:secure_url', property: 'og:image:secure_url', content: '~/assets/uploads/og-image.png' },
-      { hid: 'og:image:alt', property: 'og:image:alt', content: 'Kenan DUFRENE - Portfolio' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CDomine%3Aregular%2C700#038;subset=latin,latin-ext' }
-    ]
+  head() {
+    return {
+      title: 'Kenan DUFRENE - Portfolio',
+      htmlAttrs: {
+        lang: 'fr'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: "Passionné par la programmation depuis mon enfance, je me suis orienté dans le domaine du web et plus particulièrement sur les technologies Javascript." },
+        // twitter
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+        { hid: 'twitter:title', name: 'twitter:title', content: 'Kenan DUFRENE - Développeur web Front-End' },
+        { hid: 'twitter:description', name: 'twitter:description', content: "Passionné par la programmation depuis mon enfance, je me suis orienté dans le domaine du web et plus particulièrement sur les technologies Javascript." },
+        { hid: 'twitter:image', name: 'twitter:image', content: require('~/assets/uploads/og-image.png') },
+        { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: 'Kenan DUFRENE - Développeur web Front-End' },
+        /// open-graph
+        { hid: 'og:title', property: 'og:title', content: 'Kenan DUFRENE - Développeur web Front-End' },
+        { hid: 'og:description', property: 'og:description', content: "Passionné par la programmation depuis mon enfance, je me suis orienté dans le domaine du web et plus particulièrement sur les technologies Javascript." },
+        { hid: 'og:image', property: 'og:image', content: require('~/assets/uploads/og-image.png') },
+        { hid: 'og:image:secure_url', property: 'og:image:secure_url', content: require('~/assets/uploads/og-image.png') },
+        { hid: 'og:image:alt', property: 'og:image:alt', content: 'Kenan DUFRENE - Développeur web Front-End' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2Cregular%2Citalic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CDomine%3Aregular%2C700#038;subset=latin,latin-ext' }
+      ]
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
